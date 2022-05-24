bd76ef31b9be3caf9a1e2bec98b3926cebcba0cf
Robson Borges <robson13.bsantos@gmail.com>
2022-05-23 21:42:51 -0300

    Entendendo o que são objetos e referências a objetos

---
+++ Java OO - introdução Orientação a Objetos/src/CriaConta.java
@@ -0,0 +1,37 @@
+public class CriaConta {
+
+	public static void main(String[] args) {
+
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;		
+		System.out.println("Saldo Conta 01 -> " + primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;		
+		System.out.println("Saldo Conta 01 -> " + primeiraConta.saldo);
+
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("Saldo Conta 02 -> " + segundaConta.saldo);
+		
+		System.out.println("Titular -> " + segundaConta.titular);
+		System.out.println("Agência 02 -> " + segundaConta.agencia);
+		
+		segundaConta.agencia = 1606;
+		System.out.println("Agência 02 Mudou de Número-> " + segundaConta.agencia);
+
+		if(primeiraConta == segundaConta) {
+			System.out.println("Primeira e Segunda contas São Iguais");
+		} else {
+			System.out.println("Primeira e Segunda contas São Diferentes");
+
+		}
+
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+
+
+
+
+	}
+}
---
+++ Java OO - introdução Orientação a Objetos/src/TesteReferencias.java
@@ -0,0 +1,31 @@
+
+public class TesteReferencias {
+
+	//referência dos objetos
+	public static void main(String[] args) {
+
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("Primeira Conta...: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		System.out.println("Segunda Conta...: " + segundaConta.saldo);
+
+		segundaConta.saldo += 100;
+		
+		System.out.println("Segunda Conta...: " + segundaConta.saldo);
+		System.out.println("Primeira Conta...: " + primeiraConta.saldo);
+
+		if(primeiraConta == segundaConta) {
+			System.out.println("Primeira e Segunda contas São Iguais");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+		
+		
+	}
+
+}
